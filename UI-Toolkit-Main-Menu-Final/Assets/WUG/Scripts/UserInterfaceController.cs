d014cc310e7824cb44fd3cc993e5139473c59c52
Stacey <yecats131@gmail.com>
2022-04-10 20:08:13 -0700

    working on menu transition

--- UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/UserInterfaceController.cs
+++ UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/UserInterfaceController.cs
@@ -7,12 +7,25 @@ using System;
 
 public class UserInterfaceController : MonoBehaviour
 {
+    private enum MenuState
+    {
+        Collapsed,
+        Expanded
+    }
+
+    private MenuState _currentMenuState;
+
     private VisualElement _root;
     private VisualElement _menu;
+
     private bool expanded = true;
     private VisualElement _settingsButton;
     private VisualElement _cancelButton;
 
+    private bool _navTransitionActive;
+
+    private const string EXPANDED = "menu-expanded";
+    private const string COLLAPSED = "menu-collapsed";
 
     private void Start()
     {
@@ -24,14 +37,53 @@ public class UserInterfaceController : MonoBehaviour
         _settingsButton = mainOptions[mainOptions.Count - 2];
         _cancelButton = _menu.Q<VisualElement>("SettingsContent").Children().Last();
 
+        _menu.RegisterCallback<TransitionEndEvent>((evt) => 
+        {
+            Debug.Log("Transition End Fired");
+            if (_currentMenuState == MenuState.Collapsed)
+            {
+               _menu.EnableInClassList(COLLAPSED, false);
+
+                _currentMenuState = MenuState.Expanded;
+            }
+            else
+            {
+                _navTransitionActive = false;
+            }
+
+        });
+        
+        
         _settingsButton.RegisterCallback<MouseDownEvent>((evt) => 
         {
+            //Cannot interact with the settings button during a transition
+            if (_navTransitionActive)
+            {
+                return;
+            }
+
             Debug.Log("Settings Clicked");
+
+            _currentMenuState = MenuState.Collapsed;
+            _navTransitionActive = true;
+
+            _menu.EnableInClassList(COLLAPSED, true);
         });
 
         _cancelButton.RegisterCallback<MouseDownEvent>((evt) =>
         {
+            //Cannot interact with the settings button during a transition
+            if (_navTransitionActive)
+            {
+                return;
+            }
+
             Debug.Log("Settings Clicked");
+
+            _currentMenuState = MenuState.Collapsed;
+            _navTransitionActive = true;
+
+            _menu.EnableInClassList(COLLAPSED, true);
         });
     }
 
