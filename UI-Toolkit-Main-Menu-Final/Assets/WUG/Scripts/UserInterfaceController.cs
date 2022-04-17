cd635cf3f3608cfe35043749345bb77c1fac5b8b
Stacey <yecats131@gmail.com>
2022-04-16 20:38:53 -0700

    Need to do the widgets

--- UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/UserInterfaceController.cs
+++ UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/UserInterfaceController.cs
@@ -7,24 +7,9 @@ using System;
 
 public class UserInterfaceController : MonoBehaviour
 {
-    private enum MenuState
-    {
-        Main,
-        Settings
-    }
-
-    private MenuState _targetMenuState = MenuState.Main;
-
-    private MenuState _currentMenuState;
-
-    private VisualElement _root;
     private VisualElement _menu;
     private List<VisualElement> _mainMenuOptions;
     private VisualElement _settingsMenuOptions;
-
-    private bool expanded = true;
-    private VisualElement _settingsButton;
-    private VisualElement _cancelButton;
 
     private const string POPUP_ANIMATION = "pop-animation-hide";
     private int _mainPopupIndex = -1;
@@ -34,19 +19,18 @@ public class UserInterfaceController : MonoBehaviour
 
     private void Awake()
     {
-        _root = GetComponent<UIDocument>().rootVisualElement;
-        _menu = _root.Q<VisualElement>("Menu");
+        _menu = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("Menu");
 
         _mainMenuOptions = _menu.Q<VisualElement>("MainNav").Children().ToList();
         _settingsMenuOptions = _menu.Q<VisualElement>("SettingsNav");
 
-        _settingsButton = _mainMenuOptions.Cast<Label>().FirstOrDefault(x => x.text.ToUpper().Equals("SETTINGS"));
-        _cancelButton = _settingsMenuOptions.Children().Last();
+        //var settingsButton = _mainMenuOptions.Cast<Label>().FirstOrDefault(x => x.text.ToUpper().Equals("SETTINGS"));
+        //var cancelButton = _settingsMenuOptions.Children().Last();
 
         _menu.RegisterCallback<TransitionEndEvent>(Menu_TransitionEnd);
 
-        _settingsButton.RegisterCallback<MouseDownEvent>(Button_OnClick);
-        _cancelButton.RegisterCallback<MouseDownEvent>(Button_OnClick);
+        //settingsButton.RegisterCallback<MouseDownEvent>(Button_OnClick);
+        //cancelButton.RegisterCallback<MouseDownEvent>(Button_OnClick);
     }
 
     private IEnumerator Start()
@@ -58,27 +42,38 @@ public class UserInterfaceController : MonoBehaviour
 
     private void Menu_TransitionEnd(TransitionEndEvent evt)
     {
-        if (!evt.stylePropertyNames.Contains("opacity")) { return; }
+        if (!evt.stylePropertyNames.Contains("opacity") || _mainPopupIndex == _mainMenuOptions.Count - 1) { return; }
 
-        _firstLoadDone = _mainPopupIndex == _mainMenuOptions.Count - 1;
+        _mainPopupIndex++;
 
-        if (!_firstLoadDone)
-        {
-            _mainPopupIndex++;
+        _mainMenuOptions[_mainPopupIndex].ToggleInClassList(POPUP_ANIMATION);
 
-            _mainMenuOptions[_mainPopupIndex].ToggleInClassList(POPUP_ANIMATION);
-
-        }
-        else if (_navTransitionActive)
-        {
-            _mainMenuOptions[0].parent.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.Flex : DisplayStyle.None;
-            _settingsMenuOptions.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
-
-            _navTransitionActive = false;
-
-            _menu.ToggleInClassList(POPUP_ANIMATION);
-        }
     }
+
+
+    //private void Menu_TransitionEnd(TransitionEndEvent evt)
+    //{
+    //    if (!evt.stylePropertyNames.Contains("opacity")) { return; }
+
+    //    _firstLoadDone = _mainPopupIndex == _mainMenuOptions.Count - 1;
+
+    //    if (!_firstLoadDone)
+    //    {
+    //        _mainPopupIndex++;
+
+    //        _mainMenuOptions[_mainPopupIndex].ToggleInClassList(POPUP_ANIMATION);
+
+    //    }
+    //    else if (_navTransitionActive)
+    //    {
+    //        _mainMenuOptions[0].parent.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.Flex : DisplayStyle.None;
+    //        _settingsMenuOptions.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
+
+    //        _navTransitionActive = false;
+
+    //        _menu.ToggleInClassList(POPUP_ANIMATION);
+    //    }
+    //}
 
     private void Button_OnClick(MouseDownEvent evt)
     {
