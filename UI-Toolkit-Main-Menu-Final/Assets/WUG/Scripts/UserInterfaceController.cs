fb517a84c3003521062728057ce3e036774e78ef
yecats <yecats131@gmail.com>
2022-04-16 18:30:47 -0700

    Refactor

--- UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/UserInterfaceController.cs
+++ UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/UserInterfaceController.cs
@@ -19,7 +19,7 @@ public class UserInterfaceController : MonoBehaviour
 
     private VisualElement _root;
     private VisualElement _menu;
-    private VisualElement _mainMenuOptions;
+    private List<VisualElement> _mainMenuOptions;
     private VisualElement _settingsMenuOptions;
 
     private bool expanded = true;
@@ -37,66 +37,16 @@ public class UserInterfaceController : MonoBehaviour
         _root = GetComponent<UIDocument>().rootVisualElement;
         _menu = _root.Q<VisualElement>("Menu");
 
-        _mainMenuOptions = _menu.Q<VisualElement>("MainNav");
+        _mainMenuOptions = _menu.Q<VisualElement>("MainNav").Children().ToList();
         _settingsMenuOptions = _menu.Q<VisualElement>("SettingsNav");
 
-        _settingsButton = _mainMenuOptions.Children().Cast<Label>().FirstOrDefault(x => x.text.ToUpper().Equals("SETTINGS"));
+        _settingsButton = _mainMenuOptions.Cast<Label>().FirstOrDefault(x => x.text.ToUpper().Equals("SETTINGS"));
         _cancelButton = _settingsMenuOptions.Children().Last();
 
-        var children = _mainMenuOptions.Children().ToList();
+        _menu.RegisterCallback<TransitionEndEvent>(Menu_TransitionEnd);
 
-        _menu.RegisterCallback<TransitionEndEvent>((evt) => {
-
-            _firstLoadDone = _mainPopupIndex == _mainMenuOptions.childCount - 1;
-
-            if (!evt.stylePropertyNames.Contains("opacity")) { return; }
-
-            if (!_firstLoadDone)
-            {
-                _mainPopupIndex++;
-
-                _mainMenuOptions[_mainPopupIndex].ToggleInClassList(POPUP_ANIMATION);
-
-            }
-            else if (_navTransitionActive)
-            {
-                _mainMenuOptions.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.Flex : DisplayStyle.None;
-                _settingsMenuOptions.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
-
-                _navTransitionActive = false;
-
-                _menu.ToggleInClassList(POPUP_ANIMATION);
-            }
-        });
-
-
-        _settingsButton.RegisterCallback<MouseDownEvent>((evt) =>
-        {
-            //Cannot interact with the settings button during a transition
-            if (_navTransitionActive)
-            {
-                return;
-            }
-
-            _navTransitionActive = true;
-
-            _menu.ToggleInClassList(POPUP_ANIMATION);
-
-        });
-
-        _cancelButton.RegisterCallback<MouseDownEvent>((evt) =>
-        {
-            //Cannot interact with the settings button during a transition
-            if (_navTransitionActive)
-            {
-                return;
-            }
-
-            _navTransitionActive = true;
-
-            _menu.ToggleInClassList(POPUP_ANIMATION);
-
-        });
+        _settingsButton.RegisterCallback<MouseDownEvent>(Button_OnClick);
+        _cancelButton.RegisterCallback<MouseDownEvent>(Button_OnClick);
     }
 
     private IEnumerator Start()
@@ -106,14 +56,40 @@ public class UserInterfaceController : MonoBehaviour
         _menu.ToggleInClassList(POPUP_ANIMATION);
     }
 
-
-    // Start is called before the first frame update
-    [ContextMenu("Animate")]
-    public void AnimateTest()
+    private void Menu_TransitionEnd(TransitionEndEvent evt)
     {
-        _menu.EnableInClassList("menu-expanded", !expanded);
-        _menu.EnableInClassList("menu-collapsed", expanded);
+        if (!evt.stylePropertyNames.Contains("opacity")) { return; }
 
-        expanded = !expanded;
+        _firstLoadDone = _mainPopupIndex == _mainMenuOptions.Count - 1;
+
+        if (!_firstLoadDone)
+        {
+            _mainPopupIndex++;
+
+            _mainMenuOptions[_mainPopupIndex].ToggleInClassList(POPUP_ANIMATION);
+
+        }
+        else if (_navTransitionActive)
+        {
+            _mainMenuOptions[0].parent.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.Flex : DisplayStyle.None;
+            _settingsMenuOptions.style.display = _settingsMenuOptions.style.display == DisplayStyle.Flex ? DisplayStyle.None : DisplayStyle.Flex;
+
+            _navTransitionActive = false;
+
+            _menu.ToggleInClassList(POPUP_ANIMATION);
+        }
+    }
+
+    private void Button_OnClick(MouseDownEvent evt)
+    {
+        //Cannot interact with the settings button during a transition
+        if (_navTransitionActive)
+        {
+            return;
+        }
+
+        _navTransitionActive = true;
+
+        _menu.ToggleInClassList(POPUP_ANIMATION);
     }
 }
